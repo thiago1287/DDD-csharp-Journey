d13851d0df3c29d6c90cf6c4b4cad3bacfb550a1
Thiago <thiagosilva1@unipam.edu.br>
2024-07-19 21:57:39 -0300

    Add project files.

---
+++ src/Journey.Application/UseCases/Activities/Delete/DeleteActivityForTripUseCase.cs
@@ -0,0 +1,34 @@
+﻿using Journey.Communication.Responses;
+using Journey.Exception;
+using Journey.Exception.ExceptionsBase;
+using Journey.Infrastructure;
+
+namespace Journey.Application.UseCases.Trips.Delete
+{
+    public class DeleteActivityForTripUseCase
+    {
+        public ResponseActivityJson Execute(Guid tripId, Guid activityId)
+        {
+            var dbContext = new JourneyDbContext();
+
+            var activity = dbContext
+             .Activities
+             .FirstOrDefault(activity => activity.Id == activityId && activity.TripId == tripId);
+
+            if (activity is null)
+            {
+                throw new NotFoundException(ResourceErrorMessages.TRIP_NOT_FOUND);
+            }
+
+            dbContext.Activities.Remove(activity);
+            dbContext.SaveChanges();
+
+            return new ResponseActivityJson
+            {
+                Id = activity.Id,
+                Name = activity.Name,
+                Date = activity.Date,
+            };
+        }
+    }
+}
